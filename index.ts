1878fc353c89dc823d4ee3c4a9906de4fe2435ce
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-10-19 23:12:45 +0200

    Part 9.9 done

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
